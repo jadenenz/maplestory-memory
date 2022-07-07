d381e487c4d2d90a6e8c50d28305f568c8c43613
Jaden <jenzbr@gmail.com>
2022-07-07 10:47:12 -0500

    Updates

---
+++ precache-manifest.ab262d7d04e024d0db4e6a45d7d3e6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19338614673c0423f3d7",
+    "url": "/maplestory-memory/static/css/main.729d6fd4.chunk.css"
+  },
+  {
+    "revision": "19338614673c0423f3d7",
+    "url": "/maplestory-memory/static/js/main.19338614.chunk.js"
+  },
+  {
+    "revision": "bf4f215fd5a24fb4644a",
+    "url": "/maplestory-memory/static/js/1.bf4f215f.chunk.js"
+  },
+  {
+    "revision": "7a1031f297d2b9226f22",
+    "url": "/maplestory-memory/static/js/runtime~main.7a1031f2.js"
+  },
+  {
+    "revision": "f6da061196e890e7ab2beeb0a46d4794",
+    "url": "/maplestory-memory/static/media/pianus.f6da0611.png"
+  },
+  {
+    "revision": "8d163d682624f7966aea048c32072f07",
+    "url": "/maplestory-memory/static/media/thanatos.8d163d68.gif"
+  },
+  {
+    "revision": "4c9edf288a9f7811daf94107a0eb70cc",
+    "url": "/maplestory-memory/index.html"
+  }
+];
